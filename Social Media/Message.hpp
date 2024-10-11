9db42341a3368ea12f4c9e0f896ea5f11c8026c7
NITHISH <108284403+NithishBalaji22@users.noreply.github.com>
2024-10-11 16:39:09 +0530

    Update Message.hpp

--- Social Media/Message.hpp
+++ Social Media/Message.hpp
@@ -1,36 +1,3 @@
-// #pragma once
-// #include<iostream>
-// using namespace std;
-
-// class Message{
-//     private:
-//         static int messageId;
-//         string userName;
-//         string chat;
-
-//     public:
-//         Message(string userName, string chat){
-//             messageId = messageId++;
-//             this->userName = userName;
-//             this->chat = chat;
-//         }
-
-//         int getMessageId(){
-//             return messageId;
-//         }
-
-//         void editMessage(string& newChat){
-//             chat = newChat;
-//         }
-
-//         void deleteMessage(){
-//             chat = "[deleted]";
-//         }
-
-// };
-
-// int Message::messageId= 1;
-
 #pragma once
 #include <iostream>
 using namespace std;
