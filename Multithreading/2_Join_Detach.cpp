d5a0ad59afb52bc7c81e88fd6eb041313c71b474
NITHISH <108284403+NithishBalaji22@users.noreply.github.com>
2024-11-12 13:48:35 +0530

    Update 2_Join_Detach.cpp

--- Multithreading/2_Join_Detach.cpp
+++ Multithreading/2_Join_Detach.cpp
@@ -1,61 +1,61 @@
-// TOPIC: Use of join(), detach() and joinable() in Thread In C++ (c++11, 14, 20, 23)
+TOPIC: Use of join(), detach() and joinable() in Thread In C++ (c++11, 14, 20, 23)
 
-// JOIN NOTES
-// 0. Once a thread is started we wait for this thread to finish by calling join() function on thread object
-// 1. Double join will result into program termination
-// 2. If needed we should check thread is joinable before joining ( using joinable() function)
+JOIN NOTES
+0. Once a thread is started we wait for this thread to finish by calling join() function on thread object
+1. Double join will result into program termination
+2. If needed we should check thread is joinable before joining ( using joinable() function)
 
-// DETACH NOTES
-// 0. This is used to detach newly created thread from the parent thread
-// 1. Always check before detaching a thread that is joinable otherwise we may end up double detaching ans
-//    double detach() will result into program termination
-// 2. If we have detached thread and main function is returning then the detached thread execution is suspended.
+DETACH NOTES
+0. This is used to detach newly created thread from the parent thread
+1. Always check before detaching a thread that is joinable otherwise we may end up double detaching ans
+   double detach() will result into program termination
+2. If we have detached thread and main function is returning then the detached thread execution is suspended.
 
-// NOTES:
-// Either join() or detach() should be called on thread object, otheriwise during thread object's destructor it will
-// terminate the program. Because inside destructor it checks if thread is still joinable? if yes then it terminates the program
+NOTES:
+Either join() or detach() should be called on thread object, otheriwise during thread object's destructor it will
+terminate the program. Because inside destructor it checks if thread is still joinable? if yes then it terminates the program
 
 #include <iostream>
 #include <thread>
 #include <chrono>
 using namespace std;
 
-// Basic 
-// void run(int count){
-//     while(count-- > 0){
-//         cout<< count << " Zoho" << endl;
-//     }
-// }
+//Basic 
+void run(int count){
+    while(count-- > 0){
+        cout<< count << " Zoho" << endl;
+    }
+}
 
-// int main(){
-//     thread t1(run, 10);
-//     cout<< "Main" <<endl;
-//     t1.join();
-//     cout<< "Main finished"<<endl;
-//     return 0;
-// }
+int main(){
+    thread t1(run, 10);
+    cout<< "Main" <<endl;
+    t1.join();
+    cout<< "Main finished"<<endl;
+    return 0;
+}
 
-// So the "Main finished" line will only will get execute after t1 completes its tasks so printing the count with Zoho and sleeping for 5 sec (Assume some work beeing done for the 5sec)
-// If you run the below program you could see that the Main Finished will get runned after the printing + 5 Second wating period
-// The purpose of join is that it waits till the thread done with its job then start to execute the line after the t1.join() statement
+So the "Main finished" line will only will get execute after t1 completes its tasks so printing the count with Zoho and sleeping for 5 sec (Assume some work beeing done for the 5sec)
+If you run the below program you could see that the Main Finished will get runned after the printing + 5 Second wating period
+The purpose of join is that it waits till the thread done with its job then start to execute the line after the t1.join() statement
 
-// void run(int count){
-//     while(count-- > 0){
-//         cout<< count << " Zoho" << endl;
-//     }
-//     this_thread::sleep_for(chrono::seconds(5));
-// }
+void run(int count){
+    while(count-- > 0){
+        cout<< count << " Zoho" << endl;
+    }
+    this_thread::sleep_for(chrono::seconds(5));
+}
 
-// int main(){
-//     thread t1(run, 10);
-//     cout<< "Main" <<endl;
-//     t1.join();
-//     cout<< "Main finished"<<endl;
-//     return 0;
-// }
+int main(){
+    thread t1(run, 10);
+    cout<< "Main" <<endl;
+    t1.join();
+    cout<< "Main finished"<<endl;
+    return 0;
+}
 
 // Possile program termination part by double joins
-/*
+
     The thing is that it wont give you some syntax error it runs smoothly untill!!! the problem starts to occurs 
     Program Flow:
         1) Thread got created and starts to execute 
@@ -68,26 +68,26 @@ using namespace std;
             Using if(t1.joinable)
             You have written tons of code then you actually join before some hundres of codes but in your mind you thought you havent join so you included again 
             so to avoid that whenever you are joining always looks for joinable function alright !!!
-*/
 
-// void run(int count){
-//     while(count-- > 0){
-//         cout<< count << " Zoho" << endl;
-//     }
-// }
 
-// int main(){
-//     thread t1(run, 10);
-//     cout<< "Main" <<endl;
-//     t1.join();
-//     //t1.join() // To avoid such termination we use the below function to check where its joinable or not !
-//     if(t1.joinable())
-//         t1.join();
-//     cout<< "Main finished"<<endl;
-//     return 0;
-// }
+void run(int count){
+    while(count-- > 0){
+        cout<< count << " Zoho" << endl;
+    }
+}
 
-/*
+int main(){
+    thread t1(run, 10);
+    cout<< "Main" <<endl;
+    t1.join();
+    //t1.join() // To avoid such termination we use the below function to check where its joinable or not !
+    if(t1.joinable())
+        t1.join();
+    cout<< "Main finished"<<endl;
+    return 0;
+}
+
+
 If you execute the below statement you will get something like
     Output: (Could vary accoding to the thread) 
         Main
@@ -105,24 +105,24 @@ If you execute the below statement you will get something like
 
             The created thread will only be alive untill the main thread is alive if main thread got finished and the other threads running that doesnt belongs to the main thread
         then it will get terminated as the thread isnt belongs to the main thread 
-*/
 
-// void run(int count){
-//     while(count-- > 0){
-//         cout<< count << " Zoho" << endl;
-//     }
-//     this_thread::sleep_for(chrono::seconds(5));
-// }
 
-// int main(){
-//     thread t1(run, 10);
-//     cout<< "Main" <<endl;
-//     t1.detach();
-//     cout<< "Main finished"<<endl;
-//     return 0;
-// }
+void run(int count){
+    while(count-- > 0){
+        cout<< count << " Zoho" << endl;
+    }
+    this_thread::sleep_for(chrono::seconds(5));
+}
 
-/*  
+int main(){
+    thread t1(run, 10);
+    cout<< "Main" <<endl;
+    t1.detach();
+    cout<< "Main finished"<<endl;
+    return 0;
+}
+
+ 
     Below Code 
     Output: (Could vary accoding to the thread)
         Main
@@ -142,44 +142,44 @@ If you execute the below statement you will get something like
             Because its the same case as before but we are making the parent thread to sleep for 5 Seconds in the mean time of that 5 seconds the child thread t1 
         gets created and executed how cool is that !!
 
-*/
 
-// void run(int count){
-//     while(count-- > 0){
-//         cout<< count << " Zoho" << endl;
-//     }
-//     cout<< "Thread Finished !"<< endl;
-// }
 
-// int main(){
-//     thread t1(run, 10);
-//     cout<< "Main" <<endl;
-//     t1.detach();
-//     this_thread::sleep_for(chrono::seconds(5));
-//     cout<< "Main finished"<<endl; // Waits for 5 Seconds then it execute this statement
-//     return 0;
-// }
+void run(int count){
+    while(count-- > 0){
+        cout<< count << " Zoho" << endl;
+    }
+    cout<< "Thread Finished !"<< endl;
+}
+
+int main(){
+    thread t1(run, 10);
+    cout<< "Main" <<endl;
+    t1.detach();
+    this_thread::sleep_for(chrono::seconds(5));
+    cout<< "Main finished"<<endl; // Waits for 5 Seconds then it execute this statement
+    return 0;
+}
 
 
 // Problem : Using detach more than once
 
-// void run(int count){
-//     while(count-- > 0){
-//         cout<< count << " Zoho" << endl;
-//     }
-//     cout<< "Thread Finished !"<< endl;
-// }
+void run(int count){
+    while(count-- > 0){
+        cout<< count << " Zoho" << endl;
+    }
+    cout<< "Thread Finished !"<< endl;
+}
 
-// int main(){
-//     thread t1(run, 10);
-//     cout<< "Main" <<endl;
-//     t1.detach();
-//     // t1.detach() problem
-//     if(t1.joinable())
-//         t1.detach(); // Only if the t1 is joinable we can detach right think intuitively
-//     this_thread::sleep_for(chrono::seconds(5));
-//     cout<< "Main finished"<< endl; // Waits for 5 Seconds then it execute this statement
-//     return 0;
-// }
+int main(){
+    thread t1(run, 10);
+    cout<< "Main" <<endl;
+    t1.detach();
+    // t1.detach() problem
+    if(t1.joinable())
+        t1.detach(); // Only if the t1 is joinable we can detach right think intuitively
+    this_thread::sleep_for(chrono::seconds(5));
+    cout<< "Main finished"<< endl; // Waits for 5 Seconds then it execute this statement
+    return 0;
+}
 
 
