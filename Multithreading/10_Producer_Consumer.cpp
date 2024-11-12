83aeeb5286478821ff81611c4fd509c6faef3574
NITHISH <108284403+NithishBalaji22@users.noreply.github.com>
2024-11-12 13:51:50 +0530

    Update 10_Producer_Consumer.cpp

--- Multithreading/10_Producer_Consumer.cpp
+++ Multithreading/10_Producer_Consumer.cpp
@@ -5,6 +5,32 @@
 #include <condition_variable>
 using namespace std;
 
+TOPIC: Producer/Consumer OR Bounded Buffer Problem In C++11 Threading
+
+THE PROBLEM STATEMENT:
+1. Producer will produce and consumer will consume with synchronization of a common buffer.
+2. Until producer thread produces any data consumer thread can't consume.
+3. Threads will use condition_variable to notify each other.
+4. We need mutex if we use condition_variable because CV waits on mutex.
+5. This is one of the example of producer consumer there are many.
+
+PRODUCER thread steps:
+1. lock mutex, if success then go ahead otherwise wait for mutex to get free.
+2. check if buffer is full and if it is full then unlock mutex and sleep, if not then go ahead and produce.
+3. insert item in buffer.
+4. unlock mutex. 
+5. notify consumer.
+
+CONSUMER thread steps:
+1. lock mutex, if success then go ahead and consume otherwise wait for mutex to get free.
+2. check if buffer is empty and if it is, then unlock the mutex and sleep, if not thean go ahead and consume.
+3. consume item from buffer.
+4. unlock mutex.
+5. notify producer.
+
+IMP:
+If you are talking about producer consumer then they have to notify each other.
+
 vector<int> buffer;
 mutex mtx;
 condition_variable cond;
