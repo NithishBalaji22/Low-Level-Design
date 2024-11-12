2712804b6fd79cf192cd0e40245e2378be06ae7e
NITHISH <108284403+NithishBalaji22@users.noreply.github.com>
2024-11-12 13:47:17 +0530

    Update 1_Introduction.cpp

--- Multithreading/1_Introduction.cpp
+++ Multithreading/1_Introduction.cpp
@@ -1,22 +1,22 @@
-// TOPIC: Introduction to thread in c++ (c++11)
+TOPIC: Introduction to thread in c++ (c++11)
 
-// QUESTIONS
-// 1. What do you understand by thread and give one example in C++ ?
+QUESTIONS
+1. What do you understand by thread and give one example in C++ ?
 
-// ANSWER
-// 1. In every application there is a default thread which is main(), in side this we create other threads.
-// 2. A thread is also known as lightweight process. Idea is achieve parallelism by dividing a process into multiple threads. 
-//    For example:
-//      a. The browser has multiple tabs that can be different threads. 
-//      b. MS Word must be using multiple threads, one thread to format the text, another thread to process inputs (spell checker)
-//      c. Visual Studio code editor would be using threading for auto completing the code. (Intellicence)
+ANSWER
+1. In every application there is a default thread which is main(), in side this we create other threads.
+2. A thread is also known as lightweight process. Idea is achieve parallelism by dividing a process into multiple threads. 
+   For example:
+     a. The browser has multiple tabs that can be different threads. 
+     b. MS Word must be using multiple threads, one thread to format the text, another thread to process inputs (spell checker)
+     c. Visual Studio code editor would be using threading for auto completing the code. (Intellicence)
 
-// WAYS TO CREATE THREADS IN C++11
-// 1. Function Pointers
-// 2. Lambda Functions
-// 3. Functors
-// 4. Member Functions
-// 5. Static Member functions
+WAYS TO CREATE THREADS IN C++11
+1. Function Pointers
+2. Lambda Functions
+3. Functors
+4. Member Functions
+5. Static Member functions
 
 #include <iostream>
 #include <thread>
