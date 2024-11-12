a664c0b79c018d79208a121bf6c6766516597798
NITHISH <108284403+NithishBalaji22@users.noreply.github.com>
2024-11-12 13:50:30 +0530

    Update 7_Thread_Synchoronization.cpp

--- Multithreading/7_Thread_Synchoronization.cpp
+++ Multithreading/7_Thread_Synchoronization.cpp
@@ -6,23 +6,23 @@ using namespace std;
 
 int bankBalance = 0;
 
-// We expect 100 or 200 but the computer is soo fast that it outputs correctly but there synchoronization problem happens in this situation then whats
-// the solution then?! Yup!! locks
+We expect 100 or 200 but the computer is soo fast that it outputs correctly but there synchoronization problem happens in this situation then whats
+the solution then?! Yup!! locks
 
-// void addMoney(int amount){
-//     bankBalance += amount;
-// }
+void addMoney(int amount){
+    bankBalance += amount;
+}
 
-// int main(){
-//     thread t1(addMoney, 100);
-//     thread t2(addMoney, 200);
+int main(){
+    thread t1(addMoney, 100);
+    thread t2(addMoney, 200);
 
-//     t1.join();
-//     t2.join();
+    t1.join();
+    t2.join();
 
-//     cout<<"Bank Balance: "<<bankBalance<<endl;
-//     return 0;
-// }
+    cout<<"Bank Balance: "<<bankBalance<<endl;
+    return 0;
+}
 
 mutex m;
 
