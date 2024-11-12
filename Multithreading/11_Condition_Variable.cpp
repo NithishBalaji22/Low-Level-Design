145e4fd89ef7366cbc3e57217739a2cf92d8e509
NITHISH <108284403+NithishBalaji22@users.noreply.github.com>
2024-11-12 13:52:11 +0530

    Update 11_Condition_Variable.cpp

--- Multithreading/11_Condition_Variable.cpp
+++ Multithreading/11_Condition_Variable.cpp
@@ -4,6 +4,19 @@
 #include <condition_variable>
 using namespace std;
 
+
+TOPIC: Condition Variable In C++ Threading
+NOTES:
+1. Condition variables allow us to synchronize threads via notifications.
+   a. notify_one();
+   b. notify_all();
+2. You need mutex to use condition variable
+3. Condition variable is used to synchronize two or more threads.
+4. Best use case of condition variable is Producer/Consumer problem.
+5. Condition variables can be used for two purposes:
+    a. Notify other threads
+    b. Wait for some condition
+
 condition_variable cond;
 mutex mtx;
 int balance = 0;
