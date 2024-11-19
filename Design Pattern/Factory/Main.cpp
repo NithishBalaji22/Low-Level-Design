634e37d324a8d875e78f88c082c7692a9915b432
NITHISH <108284403+NithishBalaji22@users.noreply.github.com>
2024-11-19 18:57:04 +0530

    Delete Design Pattern directory

--- Design Pattern/Factory/Main.cpp
+++
@@ -1,9 +0,0 @@
-#include "VehicleFactory.hpp"
-#include "Vehicle.hpp"
-#include <iostream>
-using namespace std;
-
-int main(){
-    Vehicle* vehicle = VehicleFactory::getVehicle("Car");
-    vehicle->createVehicle();
-}
