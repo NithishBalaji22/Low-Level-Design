12da84375bd53f9fc08b869d2a4c4bd504df9fd0
NITHISH <108284403+NithishBalaji22@users.noreply.github.com>
2025-01-20 14:56:12 +0530

    Update Main.cpp

--- Design Patterns/Behavioral Design Pattern/Chain Of Responsibilities/Main.cpp
+++ Design Patterns/Behavioral Design Pattern/Chain Of Responsibilities/Main.cpp
@@ -5,14 +5,6 @@
 #include "ErrorLogProcessor.hpp"
 using namespace std;
 
-// int main(){
-//     LogProcessor* logobject = new InfoLogProcessor(new DebugLogProcessor(new DebugLogProcessor(nullptr)));
-    
-//     logobject->log(LogProcessor::INFO, "Just for Info");
-//     logobject->log(LogProcessor::DEBUG, "need to debug");
-//     logobject->log(LogProcessor::ERROR, "Exception happens");
-// }
-
 int main() {
     LogProcessor *logProcessor = new InfoLogProcessor(new DebugLogProcessor(new ErrorLogProcessor(NULL)));
     logProcessor->log(logProcessor->ERROR, "get error");
