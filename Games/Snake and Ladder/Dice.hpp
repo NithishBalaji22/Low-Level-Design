4dd965bcbbf4560c59b7e2cb5e9ac6b16b3e2473
NITHISH <108284403+NithishBalaji22@users.noreply.github.com>
2025-02-05 17:42:08 +0530

    Update Dice.hpp

--- Games/Snake and Ladder/Dice.hpp
+++ Games/Snake and Ladder/Dice.hpp
@@ -1,17 +1,16 @@
-#pragma once
 #include <iostream>
-#include <random>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
-class Dice{
+class Dice {
     public:
-        int roll(){
-            random_device rd; 
-            mt19937 gen(rd());
-            uniform_int_distribution<> dist(1, 6);
+        Dice() {
+            srand(time(0)); 
+        }
 
-            // Generate a random number between 1 and 6
-            int randomNum = dist(gen);
-            return randomNum;
+        int roll() {
+            return (rand() % 6) + 1; // Generate a number from 1 to 6
         }
 };
